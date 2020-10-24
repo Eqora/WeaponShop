018eab13c341d5b38e33ef06748c78b0aea1b5af
Eqora <keduk2014@gmail.com>
2020-10-24 13:32:39 +0300

    Some edit

--- src/main/java/practice2/weapon/AK47/AK47.java
+++ src/main/java/practice2/weapon/AK47/AK47.java
@@ -4,9 +4,7 @@ import main.java.practice2.weapon.Weapon;
 
 import java.util.Objects;
 
-/**
- * @author Lenovo on 30.09.2020
- */
+
 public class AK47 extends Weapon {
 
     private int MagazineSize;
---
+++ src/main/java/practice2/weapon/M4A1/M4A1.java
@@ -0,0 +1,13 @@
+package main.java.practice2.weapon.M4A1;
+
+
+import main.java.practice2.weapon.Weapon;
+
+public class M4A1  extends Weapon {
+
+    public M4A1(int price, int caliber) {
+        super(price, caliber);
+    }
+
+
+}
